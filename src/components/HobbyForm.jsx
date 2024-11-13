c79aff91372cec41c477cef88e13370e4f5e7a01
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-11-12 22:26:29 -0500

    added component to create hobby page

--- src/components/HobbyForm.jsx
+++ src/components/HobbyForm.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 
-const HobbyForm = ({handleSubmit,submitButtonName}) => {
-    
-  return (
-    <div>
+const HobbyForm = ({setHobbyValue,handleSubmit,submitButtonName}) => {
+    const handleChange = (e) => {
+        const {name,value} = e.target;
+        setHobbyValue((prev)=> {
+          return {
+            ...prev,
+            [name]:value,
+          }})
+      }
+  
+    return (
+    <div >
         <form onSubmit={handleSubmit}>
-        <input type="text" id="tile" name="title"  onChange={handleChange} /><br />
-        <input type="textarea" id="content" name="content"  onChange={handleChange} /><br />
-        <input type="text" id="image" name="image"  onChange={handleChange} /><br />
+            <input placeholder='Title' type="text" id="tile" name="title"  onChange={handleChange} /><br />
+            <textarea placeholder='Content (Optional)' type="textarea" id="content" name="content"  onChange={handleChange} /><br />
+            <input placeholder='Image Url' type="text" id="image" name="image"  onChange={handleChange} /><br />
 
-        <button type='submit' value="submit">{submitButtonName}</button>
+            <button type='submit' value="submit">{submitButtonName}</button>
         </form>
 
     </div>
-  )
+    )
 }
 
 export default HobbyForm
--- src/pages/CreateHobby.jsx
+++ src/pages/CreateHobby.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { useState } from 'react'
+import { supabase } from '../client'
+import HobbyForm from '../components/HobbyForm'
 const CreateHobby = () => {
     const [hobby,setHobby] = useState({title:"",likes:0,content:"",comments:[],image:""})
 
     const createHobby = async (e) => {
-        
-        
+        e.preventDefault();
+        await supabase
+        .from("Hobbies")
+        .insert({title:hobby.title,content:hobby.title,image:hobby.image})
+        .select();
+        window.location = "/";      
     }
+
   return (
     <div>
-      <h1> Create Page</h1>
+      <HobbyForm
+      handleSubmit={createHobby}
+      submitButtonName={"Create Post"}
+      setHobbyValue={setHobby}
+      />
     </div>
   )
 }
