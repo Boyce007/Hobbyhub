36be6cf81b645ffffdf107a0db00f91336e04a81
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-11-12 21:55:09 -0500

    added navgiation to top of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRoutes } from 'react-router-dom'
 import './App.css'
-
+import Home from './pages/Home'
+import CreateHobby from './pages/CreateHobby'
+import HobbyInfo from './pages/HobbyInfo'
+import UpdateHobby from './pages/UpdateHobby'
+import { Link } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
+
+  let element = useRoutes([
+    {
+      path:'/',
+      element:<Home/>
+    },
+    {
+      path:'/create',
+      element:<CreateHobby/>
+    },
+    {
+      path:"/hobbyInfo",
+      element:<HobbyInfo/>
+    },
+    {
+      path:'update',
+      element:<UpdateHobby/>
+    }
+
+  ])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   
+    <div >
+      <p style={{ margin: 0, position: "absolute", left: 0,marginLeft:"10px" }}>Hobby Hub </p>
+      <div className='nav-container'>
+        
+        <Link
+        to='/'
+        className='custom-link'>
+            Home
+        </Link>
+        <Link to="/create" className='custom-link'> Create a Post</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {element}
+    </div>
+    
+
+    
   )
 }
 
---
+++ src/pages/HobbyInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HobbyInfo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HobbyInfo
