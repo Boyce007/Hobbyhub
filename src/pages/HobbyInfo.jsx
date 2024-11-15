ab851db429e8bf3854c8e45cbdc8567819444090
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-11-14 21:56:48 -0500

    comments can be updated with the by inputing in the input form dealing with bug of spacing for input and submit button

--- src/pages/HobbyInfo.jsx
+++ src/pages/HobbyInfo.jsx
@@ -5,11 +5,12 @@ import { getTimeSince } from '../client'
 import { supabase } from '../client'
 import './pages.css'
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import CreateIcon from '@mui/icons-material/Create';
 const HobbyInfo = () => {
   const {id} = useParams();
   const [hobby,setHobby] = useState({})
   const [hobbyLikes, setHobbyLikes] = useState(0); 
-  const [comments,setComments] = useState([]);
+  const [postComments,setPostComments] = useState([]);
   const [input,setInput] = useState();
 
   const handleChange = (e) => {
@@ -42,62 +43,68 @@ const HobbyInfo = () => {
       .from('Hobbies')
       .select("comments")
       .eq("id",id)
-      setComments(data[0].comments)
+      setPostComments(data[0].comments)
     }
     fetchComments();
 
-  },[comments])
+  },[postComments])
 
-  // const leaveAComment  = async()=> {
-  //   const {data} = await supabase
-  //   .from("Hobbies")
-  //   .update
-
-  // }
+  const leaveAComment  = async(e)=> {
+    e.preventDefault();
+     await supabase
+      .from("Hobbies")
+      .update({comments:[...postComments,input]})
+      .eq("id",id)
+  }
   
 
   
   
   
-  const likePost = async () => {
+  const likePost = async (e) => {
+    e.preventDefault();
     const newlikes = hobbyLikes +1 
     setHobbyLikes(newlikes);
     await supabase
-    .from("Hobbies")
-    .update({likes:newlikes})
-    .eq('id',hobby.id)
+      .from("Hobbies")
+      .update({likes:newlikes})
+      .eq('id',hobby.id)
 
   }
   return (
     <div className='info-container'>
-
-      <p>{getTimeSince(hobby.created_at)}</p>
-      <h2 >{hobby.title}</h2>
-      <p>{hobby.content}</p>
-      <img src={hobby.image} alt="test image" />
+      <div style={{display:"flex",flexDirection:"column",alignItems:"flex-start",marginBottom:"0px",marginTop:"0px" }} >
+ 
+        <p>{getTimeSince(hobby.created_at)}</p>
+        <h2 >{hobby.title}</h2>
+        <p>{hobby.content}</p>
+        <img src={hobby.image} alt="test image" />
+        
+      </div>
       <div className='like-button-container'>
-
         <button style={{backgroundColor:"white",marginRight:"10px"}} onClick={likePost}>
           <ThumbUpOffAltIcon />
         </button>
         <p>{hobbyLikes}</p>
       </div>
-      <div style={{margin:"10px",backgroundColor:"lightgrey",display:'flex',flexDirection:"column",width:"100vh",alignItems:"flex-start",gap:"1px",fontSize:"12px"}}>
+      <div style={{margin:"10px",backgroundColor:"lightgrey",display:'flex',flexDirection:"column",width:"100vh",height:"13vh",alignItems:"flex-start",gap:"1px",fontSize:"12px"}}>
             {
-            comments.map(comment=>(
+            postComments.map(comment=>(
                 <p key={comment} style={{marginLeft:"10px",marginBottom:"0px",marginTop:"5px"}} >
                 - {comment}
               </p>
             ))
             }
-          
-        <input
-        type="text"
-        placeholder='Leave a comment...'
-        value={input}
-        onChange={handleChange}
-        style={{width:"99vh"}}
-         />
+          <form onSubmit={leaveAComment}>
+            <input
+            type="text"
+            placeholder='Leave a comment...'
+            value={input}
+            onChange={handleChange}
+            style={{width:"85vh", margin:"10px"}}
+            />
+            <button type='submit' style={{backgroundColor:"lightgrey"}}><CreateIcon /></button>
+          </form>
       </div>
 
 
