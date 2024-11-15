2f9e6c05fad27c2cf30a15ea2d1e1e538e908607
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-11-14 22:34:19 -0500

    added form component to update posts

--- src/pages/UpdateHobby.jsx
+++ src/pages/UpdateHobby.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
+import { useState } from 'react';
+import { supabase } from '../client';
+import HobbyForm from '../components/HobbyForm';
 const UpdateHobby = () => {
+  const {id} = useParams();
+  const [hobby,setHobby] = useState({})
+  const updatehobby = async (e) => {
+        e.preventDefault();
+        await supabase
+        .from("Hobbies")
+        .update({title:hobby.title,image:hobby.image,content:hobby.content})
+        .eq('id',id)
+        window.location = "/";
+  }
   return (
     <div>
-      <h1>update hobby</h1>
+      <HobbyForm
+      handleSubmit={updatehobby}
+      submitButtonName={"Update Post"}
+      setHobbyValue={setHobby}
+      
+      
+      />
     </div>
   )
 }
